6b130e9ee667cc097efef957f5be9da98d423b60
juanm07 <juanbegalli@gmail.com>
2025-07-05 15:05:53 -0300

    Update evaluate_population.py

--- src/evaluate_population.py
+++ src/evaluate_population.py
@@ -4,7 +4,7 @@ from src.get_fitness_cgi_decode import get_fitness_cgi_decode
 
 def evaluate_population(population: List[List[str]]) -> dict:
     fitness = {}
-    # TODO: COMPLETAR
+    
     for individual in population:
         fitness_level = get_fitness_cgi_decode(individual)
         fitness[tuple(individual)] = fitness_level
