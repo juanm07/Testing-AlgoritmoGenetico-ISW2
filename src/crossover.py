7c6e110c09106845bf8ae53189d4b32deff38cd0
juanm07 <juanbegalli@gmail.com>
2025-07-05 15:05:18 -0300

    Update crossover.py

--- src/crossover.py
+++ src/crossover.py
@@ -5,7 +5,7 @@ from typing import List, Tuple
 def crossover(parent1: List[str], parent2: List[str]) -> Tuple[List[str], List[str]]:
     offspring1 = None
     offspring2 = None
-    # TODO: COMPLETAR
+
     size_parent1 = len(parent1)
     size_parent2 = len(parent2)
     crossover_point = min(size_parent1, size_parent2)//2 + 1    #Punto medio del padre más corto, así me aseguro de que haya mezcla
